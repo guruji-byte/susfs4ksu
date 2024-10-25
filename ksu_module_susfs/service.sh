4141126e7cf983657c204bf4c6688aaed7d54358
simonpunk <simonpunk2016@gmail.com>
2024-10-25 11:11:35 +0800

    ksu_module_susfs: rename function susfs_hexpatch_props() to susfs_hexpatch_prop_name(), also use system bin tools instead of magiskboot hexpatch; Update service.sh template

--- ksu_module_susfs/service.sh
+++ ksu_module_susfs/service.sh
@@ -34,3 +34,15 @@ enable_sus_su(){
 
 ## Disable susfs kernel log ##
 #${SUSFS_BIN} enable_log 0
+
+## Hexpatch prop name for newer pixel device ##
+cat <<EOF >/dev/null
+# Remember the length of search value and replace value has to be the same #
+resetprop -n "ro.boot.verifiedbooterror" "0"
+susfs_hexpatch_prop_name "ro.boot.verifiedbooterror" "verifiedbooterror" "hello_my_newworld"
+
+resetprop -n "ro.boot.verifyerrorpart" "true"
+susfs_hexpatch_prop_name "ro.boot.verifyerrorpart" "verifyerrorpart" "letsgopartyyeah"
+
+resetprop --delete "crashrecovery.rescue_boot_count"
+EOF
