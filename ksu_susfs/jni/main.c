155e2807cd4f910146b4149f4d09b4731cd28389
simonpunk <simonpunk2016@gmail.com>
2025-06-12 22:25:03 +0800

    Kill SUS_OVERLAYFS feature
    - Since this feature is not completed and 99% of the time we just umount it, so better drop it until we find a better reason to bring it back

--- ksu_susfs/jni/main.c
+++ ksu_susfs/jni/main.c
@@ -711,51 +711,46 @@ int main(int argc, char *argv[]) {
 					ptr_buf += str_len;
 				}
 				if (enabled_features & (1 << 5)) {
-					str_len = strlen("CONFIG_KSU_SUSFS_SUS_OVERLAYFS\n");
-					strncpy(ptr_buf, "CONFIG_KSU_SUSFS_SUS_OVERLAYFS\n", str_len);
-					ptr_buf += str_len;
-				}
-				if (enabled_features & (1 << 6)) {
 					str_len = strlen("CONFIG_KSU_SUSFS_TRY_UMOUNT\n");
 					strncpy(ptr_buf, "CONFIG_KSU_SUSFS_TRY_UMOUNT\n", str_len);
 					ptr_buf += str_len;
 				}
-				if (enabled_features & (1 << 7)) {
+				if (enabled_features & (1 << 6)) {
 					str_len = strlen("CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT\n");
 					strncpy(ptr_buf, "CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT\n", str_len);
 					ptr_buf += str_len;
 				}
-				if (enabled_features & (1 << 8)) {
+				if (enabled_features & (1 << 7)) {
 					str_len = strlen("CONFIG_KSU_SUSFS_SPOOF_UNAME\n");
 					strncpy(ptr_buf, "CONFIG_KSU_SUSFS_SPOOF_UNAME\n", str_len);
 					ptr_buf += str_len;
 				}
-				if (enabled_features & (1 << 9)) {
+				if (enabled_features & (1 << 8)) {
 					str_len = strlen("CONFIG_KSU_SUSFS_ENABLE_LOG\n");
 					strncpy(ptr_buf, "CONFIG_KSU_SUSFS_ENABLE_LOG\n", str_len);
 					ptr_buf += str_len;
 				}
-				if (enabled_features & (1 << 10)) {
+				if (enabled_features & (1 << 9)) {
 					str_len = strlen("CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS\n");
 					strncpy(ptr_buf, "CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS\n", str_len);
 					ptr_buf += str_len;
 				}
-				if (enabled_features & (1 << 11)) {
+				if (enabled_features & (1 << 10)) {
 					str_len = strlen("CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG\n");
 					strncpy(ptr_buf, "CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG\n", str_len);
 					ptr_buf += str_len;
 				}
-				if (enabled_features & (1 << 12)) {
+				if (enabled_features & (1 << 11)) {
 					str_len = strlen("CONFIG_KSU_SUSFS_OPEN_REDIRECT\n");
 					strncpy(ptr_buf, "CONFIG_KSU_SUSFS_OPEN_REDIRECT\n", str_len);
 					ptr_buf += str_len;
 				}
-				if (enabled_features & (1 << 13)) {
+				if (enabled_features & (1 << 12)) {
 					str_len = strlen("CONFIG_KSU_SUSFS_SUS_SU\n");
 					strncpy(ptr_buf, "CONFIG_KSU_SUSFS_SUS_SU\n", str_len);
 					ptr_buf += str_len;
 				}
-				if (enabled_features & (1 << 14)) {
+				if (enabled_features & (1 << 13)) {
 					str_len = strlen("CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT\n");
 					strncpy(ptr_buf, "CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT\n", str_len);
 					ptr_buf += str_len;
